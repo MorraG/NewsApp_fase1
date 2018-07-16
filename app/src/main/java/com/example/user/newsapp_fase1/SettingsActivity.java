803eb598abb38ba2a5f3f28351538cab6e20269a
MorraG <guidomorra13@gmail.com>
2018-07-16 21:47:31 +0200

    Adding Intent (browser) into Main Activity

--- app/src/main/java/com/example/user/newsapp_fase1/SettingsActivity.java
+++ app/src/main/java/com/example/user/newsapp_fase1/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.example.user.newsapp_fase1;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -10,18 +13,43 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.settings_main);;
+        setContentView(R.layout.settings_activity);
+
     }
 
-    private void addPreferencesFromResource(int settings_main) {
-    }
+    public static class NewsPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+            Preference oldest = findPreference(getString(R.string.settings_order_by_oldest_value));
+            bindPreferenceSummaryToValue(oldest);
+        }
 
-    public static class NewsPreferenceFragment extends PreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            preference.setSummary(getLabelGivenValue(stringValue));
+            return true;
+        }
 
-            return  false;
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences =
+                    PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+        //Helper method to retireve the label from the selected value
+        private String getLabelGivenValue(String value) {
+            String result = "All Sections";
+            String values[] = getResources().getStringArray(R.array.settings_order_by_values);
+            for (int i = 0; i < values.length; i++) {
+                if (value.equals(values[i])) {
+                    result = getResources().getStringArray(R.array.settings_order_by_labels)[i];
+                }
+            }
+            return result;
         }
     }
 }
